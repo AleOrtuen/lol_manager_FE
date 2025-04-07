345c9bbc5b8fd50133ac44e531e126814662ba59
AleOrtuen <alessio.cappelletto92@gmail.com>
2025-04-07 17:36:04 +0200

    componente modale update user; modifiche componente account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux"
 import Navbar from "./Navbar";
 import Champions from "./Champions";
+import ModaleUserData from "./ModaleUserData";
 import { EMAIL_REGEX, PASSWORD_REGEX } from "../utils/costanti";
 import { userUpdate } from "../service/userService";
 import topIco from '../img/roles/top.webp';
@@ -12,9 +13,9 @@ import supIco from '../img/roles/sup.webp';
 
 function Account() {
 
-    const user = useSelector( (state) => state.user);
-    const teams = useSelector( (state) => state.team);
-    const [form,setForm] = useState({
+    const user = useSelector((state) => state.user);
+    const teams = useSelector((state) => state.team);
+    const [form, setForm] = useState({
         idUtente: user.idUser,
         username: '',
         email: '',
@@ -23,6 +24,7 @@ function Account() {
         pRole: '',
         error: ''
     })
+    const [dataToUpdate, setDataToUpdate] = useState(null);
 
     const roleImages = {
         top: topIco,
@@ -32,98 +34,7 @@ function Account() {
         sup: supIco,
     };
 
-    const roleImage = user && user.pRole ? roleImages[user.pRole] : null; 
-
-    const handleChange = (e) => {
-        const { name, value } = e.target; 
-        setForm((prevData) => ({
-            ...prevData,
-            [name]: value 
-        }));   
-    }
-
-    // METODO PER ATTIVARE IL PULSANTE REGISTRATI CHE RICHIAMA TUTTE LE VALIDAZIONI DEGLI INPUT 
-    const validForm = () => {
-        return validUsername(form.username)
-            && validEmail(form.email)
-            && validPassword(form.password)
-            && validPassword2(form.password2)
-    }
-    // METODI DI VALIDAZIONE DEGLI INPUT 
-    const validUsername = (username) => {
-        return username !== '' 
-            && username.length >= 3
-            && username.length <= 50;
-    }
-    const validEmail = (email) => {
-        return EMAIL_REGEX.test(email)
-            && email.length <= 50;
-    }
-    const validPassword = (password) => {
-        return PASSWORD_REGEX.test(password) 
-            && password.length <= 50;
-    }
-    const validPassword2 = (password2) => {
-        return PASSWORD_REGEX.test(password2) 
-            && password2.length <= 50;
-    }
-
-    // GESTISCE GLI ERRORI DI INSERIMENTO DA STAMPARE A SCHERMO
-    const errorChange = (e) => {
-        const { name, value } = e.target; 
-        switch(name) {
-            case 'username':
-                if (!validUsername(value) && value!='') {
-                    setForm(prevForm => ({ ...prevForm, error: 'Username di almeno 3 caratteri. ' }));
-                } else {
-                    if(form.error === 'Username di almeno 3 caratteri. ') {
-                    setForm(prevForm => ({ ...prevForm, error: '' }));
-                    }
-                }
-                break;
-            case 'email':
-                if (!validEmail(value) && value!='') {
-                    setForm(prevForm => ({ ...prevForm, error: 'Email non valida. ' }));                    
-                } else {
-                    if(form.error === 'Email non valida. ') {
-                        setForm(prevForm => ({ ...prevForm, error: '' }));
-                    }
-                }
-                break;
-            case 'password', 'password2':
-                if (!validPassword(value) && value!='') {
-                    setForm(prevForm => ({ ...prevForm, error: 'La password deve avere almeno 8 caratteri, una lettera maiuscola e un numero. ' }));                    
-                } else {
-                    if(form.error === 'La password deve avere almeno 8 caratteri, una lettera maiuscola e un numero. ') {
-                        setForm(prevForm => ({ ...prevForm, error: '' }));
-                    }
-                }
-                break;
-           
-            default: break;  
-        }
-    }
-
-    const utenteUpdate = () => {
-        console.log(user.email)
-        console.log(user.username)
-        const usernameV = form.username === '' ? user.username : form.username;
-        const emailV = form.email === '' ? user.email : form.email;
-        const passV = form.password === '' ? form.password2 : form.password;  
-        const userNew = {
-            idUtente: form.idUtente,
-            username: usernameV,
-            email: emailV,
-            password: passV
-        }
-        userUpdate(userNew).then ( (response) => {
-            console.log(response);
-            alert('Utente aggiornato correttamente');
-        }).catch( error => {
-            alert('Errore nell\'aggiornamento');
-            console.log(error.response.data.response);
-        })
-    }
+    const roleImage = user && user.pRole ? roleImages[user.pRole] : null;
 
     // ALLA PRESSIONE DI INVIO REGISTRA
     function handleKey(e) {
@@ -136,167 +47,126 @@ function Account() {
         <>
 
             {user && Object.keys(user).length > 0 ?
-            <div>  
-                <Navbar />
-                <header className="bg-gray bg-gradient">
-                <br/>       
-                <h1>
-                    {roleImage && (
-                    <img 
-                        src={roleImage} 
-                        style={{
-                        width: '50px', 
-                        height: '50px', 
-                        maxWidth: '50px', 
-                        maxHeight: '50px',
-                        marginRight: '5px',
-                        marginBottom: '6px'
-                        }} 
-                        alt="Role icon"
-                    />
-                    )}
-                    {user.username}
-                </h1>
-                {teams.map( (team) => 
                 <div>
-                    <span>{team.name}</span>
-                    <br/>
-                </div>
-                )}
-                <br/>
-
-                <button class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dati personali
-                </button>
-                <ul class="dropdown-menu bg-dark">
-                    <li><a class="dropdown-item text-light" href="#">{user.username} </a></li>
-                    <li><a class="dropdown-item text-light" href="#">{user.email}</a></li>
-                    <li><a class="dropdown-item text-light" href="#">{user.pRole}</a></li>
-                    <li><a class="dropdown-item text-light" href="#">Password</a></li>
-                </ul>
-
-                <br/><br/>
-                <div className="container-fluid">
-                    <div className="row justify-content-center">
-                        <div  
-                            className="col p-1" 
-                            style={{
-                                // height: '50vh',
-                                marginLeft: '10%',
-                                marginRight: '10%'
-                            }}
-                        >
-                            <div
-                                className="rounded-top d-flex flex-column h-100"
-                                style={{
-                                    border: '5px solid #242424',
-                                }}
-                            >
-                                <div className="bg-dark text-white text-center p-2">
-                                    Champion Pool
+                    <Navbar />
+                    <header className="bg-gray bg-gradient">
+                        <br />
+                        <h1>
+                            {roleImage && (
+                                <img
+                                    src={roleImage}
+                                    style={{
+                                        width: '50px',
+                                        height: '50px',
+                                        maxWidth: '50px',
+                                        maxHeight: '50px',
+                                        marginRight: '5px',
+                                        marginBottom: '6px'
+                                    }}
+                                    alt="Role icon"
+                                />
+                            )}
+                            {user.username}
+                        </h1>
+                        {teams && teams.length > 0 ?
+                            (teams.map((team) =>
+                                <div>
+                                    <span>{team.name}</span>
+                                    <br />
                                 </div>
-                                <div className="flex-grow-1 overflow-auto p-2 text-center"
-                                        style={{
-                                        display: 'flex',
-                                        flexWrap: 'wrap',
-                                        gap: '3px',
-                                        justifyContent: 'center',
-                                        alignContent: 'flex-start'
-                                        }}
+                            ))
+                            : null
+                        }
+
+                        <br />
+
+                        <button class="btn btn-outline-secondary btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Modifica dati personali
+                        </button>
+                        <ul class="dropdown-menu bg-dark">
+                            <li>
+                                <a
+                                    className="dropdown-item text-light"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#modal"
+                                    onClick={() => setDataToUpdate({ field: 'username', value: user.username })}
                                 >
-                                    <Champions champions={user.champions} />
+                                    <b>Username:</b> {user.username}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="dropdown-item text-light"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#modal"
+                                    onClick={() => setDataToUpdate({ field: 'email', value: user.email })}
+                                >
+                                    <b>Email:</b> {user.email}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="dropdown-item text-light"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#modal"
+                                    onClick={() => setDataToUpdate({ field: 'ruolo', value: user.pRole })}
+                                >
+                                    <b>Ruolo:</b> {user.pRole}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    className="dropdown-item text-light"
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#modal"
+                                    onClick={() => setDataToUpdate({ field: 'password', value: user.password })}
+                                >
+                                    <b>Password</b>
+                                </a>
+                            </li>
+                        </ul>
+                        <ModaleUserData toUpdate={dataToUpdate} />
+                        <br /><br />
+                        <div className="container-fluid">
+                            <div className="row justify-content-center">
+                                <div
+                                    className="col p-1"
+                                    style={{
+                                        // height: '50vh',
+                                        marginLeft: '10%',
+                                        marginRight: '10%'
+                                    }}
+                                >
+                                    <div
+                                        className="rounded-top d-flex flex-column h-100"
+                                        style={{
+                                            border: '5px solid #242424',
+                                        }}
+                                    >
+                                        <div className="bg-dark text-white text-center p-2">
+                                            Champion Pool
+                                        </div>
+                                        <div className="flex-grow-1 overflow-auto p-2 text-center"
+                                            style={{
+                                                display: 'flex',
+                                                flexWrap: 'wrap',
+                                                gap: '3px',
+                                                justifyContent: 'center',
+                                                alignContent: 'flex-start'
+                                            }}
+                                        >
+                                            <Champions champions={user.champions} />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </header>
                 </div>
-
-                {/* <h5>Aggiorna dati</h5>
-                <br/> */}
-                {/* <div class="row">       
-                    <div class="col-md-4 offset-md-4 col-sm-8 offset-sm-2">
-                        <form>
-                            <div class="form-floating mb-3">
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    value={form.username}
-                                    placeholder="Username"
-                                    onChange={(e) => handleChange(e)}
-                                    onKeyDown={handleKey}
-                                    onBlur={(e) => errorChange(e)}
-                                    style={{
-                                    borderColor: validUsername(form.username) ? 'green' : (form.username !== '' ? 'red' : '')
-                                    }}
-                                />
-                                <label htmlFor="username">Username</label>
-                            </div>              
-                            <div class="form-floating mb-3">
-                                <input
-                                    class="form-control"
-                                    type="text"
-                                    name="email"
-                                    id="email"
-                                    value={form.email}
-                                    placeholder="Email"
-                                    onChange={(e) => handleChange(e)}
-                                    onKeyDown={handleKey}
-                                    onBlur={(e) => errorChange(e)}
-                                    style={{
-                                    borderColor: validEmail(form.email) ? 'green' : (form.email !== '' ? 'red' : '')
-                                    }}
-                                />
-                                <label htmlFor="email">Email</label>
-                            </div> 
-                            <div class="form-floating mb-3">
-                                <input
-                                    class="form-control"
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    value={form.password}
-                                    placeholder="Password"
-                                    onChange={(e) => handleChange(e)}
-                                    onKeyDown={handleKey}
-                                    onBlur={(e) => errorChange(e)}
-                                    style={{
-                                    borderColor: validPassword(form.password) ? 'green' : (form.password !== '' ? 'red' : '')
-                                    }}
-                                />
-                                <label htmlFor="password">Vecchia password</label>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input
-                                    class="form-control"
-                                    type="password"
-                                    name="password2"
-                                    id="password2"
-                                    value={form.password2}
-                                    placeholder="Password"
-                                    onChange={(e) => handleChange(e)}
-                                    onKeyDown={handleKey}
-                                    onBlur={(e) => errorChange(e)}
-                                    style={{
-                                    borderColor: validPassword(form.password) ? 'green' : (form.password !== '' ? 'red' : '')
-                                    }}
-                                />
-                                <label htmlFor="password">Nuova Password</label>
-                            </div>
-                        </form>
-                        <div>
-                            &nbsp;{form.error}
-                        </div> <br/>
-                        <button class="btn btn-outline-secondary btn-lg" onClick={() => utenteUpdate()}>Aggiorna</button> <br/><br/>
-                    </div>
-                </div>  */}
-                </header>               
-            </div>
-            : <h3>Utente non loggato</h3>
+                : <h3>Utente non loggato</h3>
             }
         </>
-  
+
 
 
     )
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-import { USER_AUTH, USER_FIND_TEAMS, USER_SAVE, USER_UPDATE } from "../utils/endpoint";
+import { USER_AUTH, USER_FIND_ID, USER_FIND_TEAMS, USER_SAVE, USER_UPDATE } from "../utils/endpoint";
+
 
 export function userSave(user) {
     return axios.post(USER_SAVE, user);
 }
 
 export function userUpdate(user) {
-    return axios.post(USER_UPDATE, user);
+    return axios.put(USER_UPDATE, user);
+}
+
+export function userFindById(idUser) {
+    return axios.get(`${USER_FIND_ID}${idUser}`)
 }
 
 export function userFindTeams(idUser) {
